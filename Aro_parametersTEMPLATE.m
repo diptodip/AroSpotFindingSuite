66238e9f88e5c35cb8428c25e1d0c3cb3f74aedb
Scott Rifkin <sarifkin@ucsd.edu>
2015-02-18 16:48:42 -0800

    Moved statsToUse declaration to Aro_parameters
    The decision of which stats to use should be in Aro_parameters so that there is
    minimal messing the user has to do with the core files.
    This also allows the user to run an analysis with a different (perhaps smaller) set of statistics
    in a new analysis directory just by modifying the parameter file
    and not touching the actual function at all

--- Aro_parametersTEMPLATE.m
+++ Aro_parametersTEMPLATE.m
@@ -13,7 +13,7 @@
 %                which functions use them.  Each file only reads in the
 %                ones it needs based on the value of callingFunction which
 %                is set in the calling function and is the function's name
-%				 
+%
 %				NestedDirectoryStructure
 %					tells all functions where to find things
 %					assumes that you are running it from the top directory
@@ -37,65 +37,65 @@ dyesUsed={'a594','cy','tmr'};
 isdapi='dapi'; %or '' if no dapi
 
 if strcmp(nestedOrFlatDirectoryStructure,'nested')
-topdir=pwd;
-ImageDir='ImageData';
-
-SegmentationMaskDir= 'SegmentationMasks';
-if ~exist(SegmentationMaskDir,'dir')
-	mkdir(SegmentationMaskDir);
-end;
-
-
-AnalysisDir= 'Analysis1';
-if ~exist(AnalysisDir,'dir')
-	mkdir(AnalysisDir);
-end;
-
-PlotDir=[AnalysisDir filesep 'Plots'];
-if ~exist(PlotDir,'dir')
-	mkdir(PlotDir);
-end;
-
-WormGaussianFitDir=[AnalysisDir filesep 'WormGaussianFit'];
-if ~exist(WormGaussianFitDir,'dir')
-	mkdir(WormGaussianFitDir);
-	for i=1:length(dyesUsed)
-		mkdir([WormGaussianFitDir filesep dyesUsed{i}]);
-	end;
-end;
-
-SpotStatsDir=[AnalysisDir filesep 'SpotStats'];
-if ~exist(SpotStatsDir,'dir')
-	mkdir(SpotStatsDir);
-	for i=1:length(dyesUsed)
-		mkdir([SpotStatsDir filesep dyesUsed{i}]);
-	end;
-end;
-
-SegStacksDir=[AnalysisDir filesep 'SegStacks'];
-if ~exist(SegStacksDir,'dir')
-	mkdir(SegStacksDir);
-	for i=1:length(dyesUsed)
-		mkdir([SegStacksDir filesep dyesUsed{i}]);
-	end;
-	if ~isempty(isdapi)
-		mkdir([SegStacksDir filesep isdapi]);
-	end;
-end;
-
-TrainingSetDir=[AnalysisDir filesep 'TrainingSets'];
-if ~exist(TrainingSetDir,'dir')
-	mkdir(TrainingSetDir);
-end;
+    topdir=pwd;
+    ImageDir='ImageData';
+    
+    SegmentationMaskDir= 'SegmentationMasks';
+    if ~exist(SegmentationMaskDir,'dir')
+        mkdir(SegmentationMaskDir);
+    end;
+    
+    
+    AnalysisDir= 'Analysis1';
+    if ~exist(AnalysisDir,'dir')
+        mkdir(AnalysisDir);
+    end;
+    
+    PlotDir=[AnalysisDir filesep 'Plots'];
+    if ~exist(PlotDir,'dir')
+        mkdir(PlotDir);
+    end;
+    
+    WormGaussianFitDir=[AnalysisDir filesep 'WormGaussianFit'];
+    if ~exist(WormGaussianFitDir,'dir')
+        mkdir(WormGaussianFitDir);
+        for i=1:length(dyesUsed)
+            mkdir([WormGaussianFitDir filesep dyesUsed{i}]);
+        end;
+    end;
+    
+    SpotStatsDir=[AnalysisDir filesep 'SpotStats'];
+    if ~exist(SpotStatsDir,'dir')
+        mkdir(SpotStatsDir);
+        for i=1:length(dyesUsed)
+            mkdir([SpotStatsDir filesep dyesUsed{i}]);
+        end;
+    end;
+    
+    SegStacksDir=[AnalysisDir filesep 'SegStacks'];
+    if ~exist(SegStacksDir,'dir')
+        mkdir(SegStacksDir);
+        for i=1:length(dyesUsed)
+            mkdir([SegStacksDir filesep dyesUsed{i}]);
+        end;
+        if ~isempty(isdapi)
+            mkdir([SegStacksDir filesep isdapi]);
+        end;
+    end;
+    
+    TrainingSetDir=[AnalysisDir filesep 'TrainingSets'];
+    if ~exist(TrainingSetDir,'dir')
+        mkdir(TrainingSetDir);
+    end;
 else
-topdir=''; %want relative paths without extra cruff
-ImageDir=topdir;
-WormGaussianFitDir=topdir;
-SpotStatsDir=topdir;
-SegStacksDir=topdir;
-SegmentationMaskDir=topdir;
-TrainingSetDir=topdir;
-PlotDir=topdir;
+    topdir=''; %want relative paths without extra cruff
+    ImageDir=topdir;
+    WormGaussianFitDir=topdir;
+    SpotStatsDir=topdir;
+    SegStacksDir=topdir;
+    SegmentationMaskDir=topdir;
+    TrainingSetDir=topdir;
+    PlotDir=topdir;
 end;
 
 
@@ -136,11 +136,11 @@ switch callingFunction
         stepFactor=1;  %1 or more
         %%%%%%%%
         % What should the confidence interval width be?
-        intervalWidth=95;   % (0 < < 100] 
+        intervalWidth=95;   % (0 < < 100]
         
     case 'classifySpots'
         % What should the confidence interval width be?
-        intervalWidth=95;% (0 < < 100] 
+        intervalWidth=95;% (0 < < 100]
         
     case 'evalFISHStacks'
         % This function builds the list of regional maxima in an image
@@ -170,6 +170,17 @@ switch callingFunction
     case 'makeSpotCountInterval'
         %bootstrap repetitions for the interval
         nBoots=1000; %should be a large number
+    case 'createSpotTrainingSet'
+        statsToUse = {'intensity';'totalHeight';'estimatedFloor';'scnmse';'scnrmse';'scr';'scd';'sce';...
+            'prctile_50';'prctile_60';'prctile_70';'prctile_80';'prctile_90';...
+            'fraction_center';'fraction_plusSign';'fraction_3box';'fraction_5star';'fraction_5box';'fraction_7star';'fraction_3ring';...
+            'raw_center';'raw_plusSign';'raw_3box';'raw_5star';'raw_5box';'raw_7star';'raw_3ring';'total_area';...
+            'sv1';'sv2';'sv3';'sv4';'sv5';...
+            'absDeltaPlusSign';'deltaPlusSign';'absPlusSignDelta';'plusSignPvalue';...
+            'absDeltaStarSign';'deltaStarSign';'absStarSignDelta';'starSignPvalue';...
+            'absDeltaCenterBox';'deltaCenterBox';'absCenterBoxDelta';'centerBoxPvalue';'ratioSigmaXY';...
+            'totalAreaRandPvalue';'cumSumPrctile90RP';'cumSumPrctile70RP';'cumSumPrctile50RP';'cumSumPrctile30RP';...
+            'cumSumPrctile90';'cumSumPrctile70';'cumSumPrctile50';'cumSumPrctile30'};
     otherwise
 end;
 
