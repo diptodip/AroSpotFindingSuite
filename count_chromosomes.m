bf4cafb19a81d5103ccccf18e8368368ef8f9da8
Diptodip Deb <d2d.tar.gz@gmail.com>
2016-10-02 21:36:45 -0400

    Chromosome counting implemented.

---
+++ count_chromosomes.m
@@ -0,0 +1,85 @@
+function [current_count] = count_chromosomes(filename, maskname)
+
+info = imfinfo(filename);
+
+I = imread(filename, 1);
+mask = imread(maskname);
+
+bg_brightness = mean(mean(I));
+
+rows = size(I, 1);
+cols = size(I, 2);
+
+sensitive = false;
+
+if rows >= 1000 || cols >= 1000
+    sensitive = true;
+end
+
+num_frames = length(info);
+
+totals = [num_frames, 1];
+
+start = ((num_frames - 1)/2) + 1;
+ending = num_frames - 1;
+
+for i = start:ending
+    I = imread(filename, i);
+    total = sum(sum(I));
+    totals(i, 1) = total;
+end
+
+sigma = std(totals, 0, 1);
+
+dim = false;
+
+if (sigma(1)/bg_brightness) < 3.000e+03
+    dim = true;
+end
+
+decrementer = ones(rows, cols);
+seg_I = ones(rows, cols);
+
+threshold = 2.5;
+level = threshold * bg_brightness;
+
+if ~dim
+    threshold = 2.5;
+    if sensitive
+        threshold = 3.0;
+    end
+    level = threshold * bg_brightness;
+
+else
+    threshold = 1.7;
+    level = threshold * bg_brightness;
+end
+
+previous_count = 0;
+current_count = 0;
+
+display = seg_I;
+
+for i = start:ending
+    I = imread(filename, i);
+    I = imgaussfilt(I, 3);
+    seg_I = imquantize(I, level);
+    seg_I = seg_I - decrementer;
+    if sum(sum(seg_I)) > 0.5 * rows * cols
+        seg_I = ~seg_I;
+    end
+    if i == (start + ending) / 2
+        display = seg_I;
+    end
+    seg_I(~mask) = 0;
+    current = length(bwboundaries(seg_I));
+    if current > previous_count
+        current_count = current_count + (current - previous_count);
+    end
+    previous_count = current;
+end
+
+disp(current_count);
+
+clear;
+return;
---
+++ count_chromosomes_directory.m
@@ -0,0 +1,36 @@
+function [] = count_chromosomes_directory()
+run('Aro_parameters.m');
+
+dye = dyesUsed{1};
+if ~exist([ImageDir filesep dye], 'dir')
+    disp('[ERROR] The images have not been trimmed/placed in the proper folders! Please trim first.');
+    return;
+end
+current_files = dir([RawImageDir filesep '*.tif']);
+tif_files = {current_files.name};
+disp(tif_files);
+
+if isempty(tif_files)
+    disp('[ERROR] There appear to be no images in the directory for this dye! Please check your image files.');
+    return;
+end
+
+ParSegmentationMaskDir = [SegmentationMaskDir];
+ParRawImageDir = [RawImageDir];
+ParChromosomeFile = [ChromosomeFile];
+
+parfor i = 1:numel(tif_files)
+    name = tif_files{i};
+    SegmentationMaskDir = ParSegmentationMaskDir;
+    RawImageDir = ParRawImageDir;
+    ChromosomeFile = ParChromosomeFile;
+    dye_name_length = 3;
+    mask_name = strcat('Mask_', name(dye_name_length+1:length(name)-4), '_1.tif');
+    mask_full_name [SegmentationMaskDir filesep mask_name];
+    count = count_chromosomes([RawImageDir filesep name], [SegmentationMaskDir filesep mask_name]);
+    fileID = fopen(ChromosomeFile, 'a');
+    fprintf(fileID, [name(dye_name_length+1:length(name)-4) ', ' num2str(count) '\n']);
+    fclose(fileID);
+end
+clear;
+return;
