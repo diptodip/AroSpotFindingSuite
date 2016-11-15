179e3ce3acc061c00354b62617d0fbb96171730b
Diptodip Deb <d2d.tar.gz@gmail.com>
2016-11-15 00:28:08 -0500

    Implemented adding chromosome count to final data file.

--- count_chromosomes_directory.m
+++ count_chromosomes_directory.m
@@ -10,29 +10,29 @@ current_files = dir([RawImageDir filesep '*.tif']);
 tif_files = {current_files.name};
 disp(tif_files);
 
+mask_files = dir([SegmentationMaskDir filesep 'Mask*.tif']);
+mask_files = {mask_files.name};
+disp(mask_files);
+
+masks_exist = (numel(mask_files) == numel(tif_files));
+
 if isempty(tif_files)
     disp('[ERROR] There appear to be no images in the directory for this dye! Please check your image files.');
     return;
-end
+else
 
-ParSegmentationMaskDir = [SegmentationMaskDir];
-ParRawImageDir = [RawImageDir];
-ParChromosomeFile = [ChromosomeFile];
-
-for i = 1:numel(tif_files)
-    name = tif_files{i};
-    hyphenated = strsplit(name, '-');
-    numstr = strsplit(hyphenated{2},'.');
-    numstr = numstr{1};
-    SegmentationMaskDir = ParSegmentationMaskDir;
-    RawImageDir = ParRawImageDir;
-    ChromosomeFile = ParChromosomeFile;
-    %mask_name = strcat('Mask_', name(dye_name_length+1:length(name)-4), '_1.tif');
-    mask_name = '0';
-    count = count_chromosomes([RawImageDir filesep name], mask_name);
-    fileID = fopen(ChromosomeFile, 'a');
-    fprintf(fileID, [hyphenated{1} ' ' numstr ', ' num2str(count) '\n']);
-    fclose(fileID);
+    for i = 1:numel(tif_files)
+        name = tif_files{i};
+        if masks_exist
+            mask_name = [SegmentationMaskDir filesep mask_files{i}];
+        else
+            mask_name = '0';
+        end
+        count = count_chromosomes([RawImageDir filesep name], mask_name);
+        fileID = fopen(ChromosomeFile, 'a');
+        fprintf(fileID, [name ', ' num2str(count) '\n']);
+        fclose(fileID);
+    end
 end
 clear;
 return;
