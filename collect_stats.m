6826be38aa355901d23ef5cd470b351af3d20111
Diptodip Deb <d2d.tar.gz@gmail.com>
2017-01-25 22:45:31 -0500

    Sorting file names before producing output file.

--- collect_stats.m
+++ collect_stats.m
@@ -4,14 +4,17 @@ run('Aro_parameters.m');
 for i = 1:numel(dyesUsed)
     dye = dyesUsed{i};
     list = dir([SpotStatsDir filesep dye filesep '*.mat']);
+    list = {list.name};
+    list = list.';
     if numel(list) > 0
+        list = natsortfiles(list);
         worm_counts = cell(size(list, 1), 4);
         for i = 1:size(list, 1)
-            disp(list(i).name);
-            load([SpotStatsDir filesep dye filesep list(i).name]);
-            f = list(i).name;
+            disp(list{i});
+            load([SpotStatsDir filesep dye filesep list{i}]);
+            f = list{i};
             end_index = length(f) - 14;
-            worm_counts{i, 1} = str2num(list(i).name(8:end_index));
+            worm_counts{i, 1} = str2num(list{i}(8:end_index));
             worm_counts{i, 2} = spotStats{1}.SpotNumEstimate;
             worm_counts{i, 3} = spotStats{1}.SpotNumRange(1);
             worm_counts{i, 4} = spotStats{1}.SpotNumRange(2);
