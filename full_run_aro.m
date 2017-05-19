67800726256fd018d732882cec78283deaa3b133
Diptodip Deb <d2d.tar.gz@gmail.com>
2017-05-19 12:23:04 -0400

    Scripts to run a full analysis on PACE

---
+++ full_run_aro.m
@@ -0,0 +1,7 @@
+p = path;
+path(p, '../AroSpotFindingSuite');
+p = path;
+path(p, '../AroSpotFindingSuite/saveastiff_2.2');
+p = path;
+path(path, '../AroSpotFindingSuite/mpiv_toolbox');
+run_analysis;
